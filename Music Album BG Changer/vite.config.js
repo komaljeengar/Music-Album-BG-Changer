c2cb9e109706dbeba39d6bf1813c30f4a5f15da1
Komal Jeengar <komaljeengar04@gmail.com>
2024-12-28 14:53:31 +0800

    Final2

--- Music Album BG Changer/vite.config.js
+++ Music Album BG Changer/vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'dist', // Ensure the output directory is correct for Vercel to find
+  },
   plugins: [react()],
-})
+});
+
